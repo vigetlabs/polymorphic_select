99a32ad002a2bc922aab2094fe9a39f6589f8e38
Chris Jones <chris.jones@viget.com>
2015-02-25 15:42:41 -0500

    Fix bug where type field for ungrouped options
    If the selected option isn't inside an optgroup, the value is determined to be undefined. .attr('value', undefined) doesn't clear the value, so make sure to use an empty string instead.

--- app/assets/javascripts/polymorphic_select.js
+++ app/assets/javascripts/polymorphic_select.js
@@ -3,7 +3,7 @@ $(function() {
     var $select    = $(this),
         marker     = $select.data('polymorphic'),
         $selected  = $select.find(':selected'),
-        type       = $selected.closest('optgroup').data('polymorphic-type');
+        type       = $selected.closest('optgroup').data('polymorphic-type') || '';
 
     $('input[data-polymorphic=' + marker + ']').attr('value', type);
   });
